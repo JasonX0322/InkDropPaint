0ec6edeae645397a71fd419af2d7079816fe289e
JasonX0322 <916295300@qq.com>
2023-08-17 16:58:16 +0800

    init

---
+++ Assets/Code/Ink.cs
@@ -0,0 +1,25 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ink : MonoBehaviour
+{
+    [SerializeField] Sprite[] sps;
+    Image img;
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        img = GetComponent<Image>();
+        for (int i = 0; i < sps.Length; i++)
+        {
+            img.sprite = sps[i];
+            yield return 0;
+        }
+        img.DOFade(0, 2).OnComplete(() =>
+        {
+            Destroy(this.gameObject);
+        });
+    }
+}
---
+++ Assets/Code/InkManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InkManager : MonoBehaviour
+{
+    [SerializeField] Object obj;
+    [SerializeField] Transform canvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            int x = Random.Range(-960, 960);
+            int y = Random.Range(-540, 540);
+            float size = Random.Range(1.0f, 2.0f);
+            GameObject go = GameObject.Instantiate(obj) as GameObject;
+            go.transform.SetParent(canvas);
+            go.transform.localScale = Vector3.one * size;
+            go.transform.localPosition = new Vector2(x, y);
+
+        }
+    }
+}
